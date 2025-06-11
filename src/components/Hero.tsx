cec265e4a94204797156a3f489084b913c7ba1eb
Muhammad Attique <https://github.com/Attique-dash>
2025-06-11 00:08:59 -0700

    Update the portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
           <h1 className="text-4xl md:text-6xl font-bold mt-4">
         Software <span className="text-blue-500">Developer</span> 
           </h1>
-          <p className='mt-4 text-lg font-light text-gray-300 text-center md:text-left'>I'm a BS Computer Science student at Superior University with a passion for AI, web development, and creative UI/UX. I love building smart, user-friendly applications that solve real-world problems and help people in meaningful ways.</p>
+          <p className='mt-4 text-lg font-light text-gray-300 text-center md:text-left'>I&apos;m a BS Computer Science student at Superior University with a passion for AI, web development, and creative UI/UX. I love building smart, user-friendly applications that solve real-world problems and help people in meaningful ways.</p>
           <div className="mt-8 space-x-4">
             <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded transition-colors">
               Contact me
