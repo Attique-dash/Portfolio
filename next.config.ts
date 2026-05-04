556eafdd30b7fe8e961ec0e92a038dbc9de8af17
chc31533-del <chc31533@gmail.com>
2026-05-04 17:53:12 +0500

    error solve

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     formats: ['image/webp', 'image/avif'],
     minimumCacheTTL: 60,
   },
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
